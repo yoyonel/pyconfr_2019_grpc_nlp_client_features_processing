3523d5f7a7e51dae199bc82b3cfa03506ea9a09a
Lionel Atty <lionel.atty@365talents.com>
2019-10-21 17:32:56 +0200

    refactoring: separate (gRPC) commands from client.

--- src/features_processing/client_rpc.py
+++ src/features_processing/client_rpc.py
@@ -13,13 +13,16 @@ import signal
 import sys
 from argparse import Namespace
 
-from google.protobuf.json_format import MessageToDict
 from pyconfr_2019.grpc_nlp.protos import TweetFeaturesService_pb2_grpc
-from pyconfr_2019.grpc_nlp.protos.Timeline_pb2 import Timeline
-from pyconfr_2019.grpc_nlp.protos.TweetFeaturesService_pb2 import ComputeGeneralSentimentOfUserRequest, DetectLanguageFromTweetTextRequest, TopUsersRequest
 from pyconfr_2019.grpc_nlp.tools.fct_logger import init_logger
 from pyconfr_2019.grpc_nlp.tools.fct_parser import EndDateType, StartDateType
 from pyconfr_2019.grpc_nlp.tools.rpc_init_stub import rpc_init_stub
+
+from features_processing.commands import (
+    detect_language_command,
+    general_sentiment_command,
+    top_user_command
+)
 
 logger = logging.getLogger(__name__)
 
@@ -126,37 +129,6 @@ def parse_arguments(args=None):
     """
     # return parsing
     return build_parser().parse_args(args)
-
-
-def top_user_command(features_rpc_stub, timeline_start, timeline_end):
-    top_user_response = features_rpc_stub.TopUsers(
-        TopUsersRequest(
-            timeline=Timeline(start=int(timeline_start.timestamp() * 1000),
-                              end=int(timeline_end.timestamp() * 1000)))
-    )
-    for i, top_user in enumerate(top_user_response, start=1):
-        logger.info(
-            f"#{i} - user_id={top_user.user_id}, nb_tweets={top_user.nb_tweets}")
-
-
-def general_sentiment_command(features_rpc_stub, user_id):
-    general_sentiment_of_user_response = features_rpc_stub.ComputeGeneralSentimentOfUser(
-        ComputeGeneralSentimentOfUserRequest(user_id=user_id))
-    general_sentiment = general_sentiment_of_user_response
-    logger.debug(f"{MessageToDict(general_sentiment)}")
-
-    polarity = 'positive' if general_sentiment.sentiment.polarity > 0.0 else 'negative'
-    logger.info(f"General tweets sentiment from user_id={user_id} => {polarity}")
-
-
-def detect_language_command(features_rpc_stub, tweet_id):
-    detect_language_response = features_rpc_stub.DetectLanguageFromTweetText(
-        DetectLanguageFromTweetTextRequest(tweet_id=tweet_id))
-
-    detect_language = detect_language_response
-
-    logger.debug(
-        f"Detect language on tweet (id={tweet_id}): {MessageToDict(detect_language)}")
 
 
 def process(args: Namespace):
---
+++ src/features_processing/commands.py
@@ -0,0 +1,36 @@
+from google.protobuf.json_format import MessageToDict
+from pyconfr_2019.grpc_nlp.protos.Timeline_pb2 import Timeline
+from pyconfr_2019.grpc_nlp.protos.TweetFeaturesService_pb2 import (ComputeGeneralSentimentOfUserRequest, DetectLanguageFromTweetTextRequest, TopUsersRequest)
+
+from features_processing.client_rpc import logger
+
+
+def top_user_command(features_rpc_stub, timeline_start, timeline_end):
+    top_user_response = features_rpc_stub.TopUsers(
+        TopUsersRequest(
+            timeline=Timeline(start=int(timeline_start.timestamp() * 1000),
+                              end=int(timeline_end.timestamp() * 1000)))
+    )
+    for i, top_user in enumerate(top_user_response, start=1):
+        logger.info(
+            f"#{i} - user_id={top_user.user_id}, nb_tweets={top_user.nb_tweets}")
+
+
+def general_sentiment_command(features_rpc_stub, user_id):
+    general_sentiment_of_user_response = features_rpc_stub.ComputeGeneralSentimentOfUser(
+        ComputeGeneralSentimentOfUserRequest(user_id=user_id))
+    general_sentiment = general_sentiment_of_user_response
+    logger.debug(f"{MessageToDict(general_sentiment)}")
+
+    polarity = 'positive' if general_sentiment.sentiment.polarity > 0.0 else 'negative'
+    logger.info(f"General tweets sentiment from user_id={user_id} => {polarity}")
+
+
+def detect_language_command(features_rpc_stub, tweet_id):
+    detect_language_response = features_rpc_stub.DetectLanguageFromTweetText(
+        DetectLanguageFromTweetTextRequest(tweet_id=tweet_id))
+
+    detect_language = detect_language_response
+
+    logger.debug(
+        f"Detect language on tweet (id={tweet_id}): {MessageToDict(detect_language)}")
